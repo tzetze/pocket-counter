b9a3895f9b9991956ec122dfc0e4810418ae04a9
cece <heilig.szabolcs@ustream.tv>
2016-07-17 14:21:00 +0200

    whitespace handling based on google coding standards, removed clutter

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ import client
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-	code = client.get_request_token(config.code_url, config.consumer_key, config.redirect_uri)
-	return redirect(
-		client.build_auth_url(config.auth_url, code, config.redirect_uri)
-	)
+    code = client.get_request_token(config.code_url, config.consumer_key, config.redirect_uri)
+    return redirect(
+        client.build_auth_url(config.auth_url, code, config.redirect_uri)
+    )
 
 @app.route("/callback")
 def callback():
-	return 'returned!'
+    return 'returned!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4242, debug=False)
--- client.py
+++ client.py
@@ -4,18 +4,21 @@ import urlparse
 
 def get_request_token(code_url, consumer_key, redirect_uri):
 
-	post_params = {
-		'consumer_key': consumer_key,
-		'redirect_uri': redirect_uri
-	}
+    post_params = {
+        'consumer_key': consumer_key,
+        'redirect_uri': redirect_uri
+    }
 
-	request = urllib2.Request(code_url, urllib.urlencode(post_params))
-	response = urllib2.urlopen(request)
+    request = urllib2.Request(code_url, urllib.urlencode(post_params))
+    response = urllib2.urlopen(request)
 
-	raw_response = response.read()
-	response_body = dict(urlparse.parse_qsl(raw_response))
-	return response_body.get('code')
+    raw_response = response.read()
+    response_body = dict(urlparse.parse_qsl(raw_response))
+    return response_body.get('code')
 
 def build_auth_url(auth_url, code, redirect_uri):
-	return auth_url + '?' + urllib.urlencode({'request_token': code, 'redirect_uri': redirect_uri})
-#	return auth_url + '?request_token=' + code + '&redirect_uri=' + redirect_uri
+    auth_params = {
+        'request_token': code,
+        'redirect_uri': redirect_uri
+    }
+    return auth_url + '?' + urllib.urlencode(auth_params)
