30693ad991b5fe7271c258b95d73c9429c24b75e
cece <heilig.szabolcs@ustream.tv>
2016-07-17 22:51:34 +0200

    not so secret

---
+++ config.py
@@ -0,0 +1,9 @@
+oauth_request_url = 'https://getpocket.com/v3/oauth/request'
+authorize_url = 'https://getpocket.com/auth/authorize'
+oauth_authorize_url = 'https://getpocket.com/v3/oauth/authorize'
+list_url = 'https://getpocket.com/v3/get'
+
+consumer_key = '56612-448efbabdf7b7eb1b133f314'
+redirect_uri = 'http://127.0.0.1:4242/callback'
+
+session_secret = 'you-should-change-it'
