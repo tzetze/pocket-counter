0122c01de075d047c01238b248d73a2b07386f34
cece <heilig.szabolcs@ustream.tv>
2016-07-17 20:25:45 +0200

    happy path: acquiring token and using it to count pocket entries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, session
 import sys
 import config
 import client
@@ -7,14 +7,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    code = client.get_request_token(config.code_url, config.consumer_key, config.redirect_uri)
+    code = client.get_request_token(config.oauth_request_url, config.consumer_key, config.redirect_uri)
+    session['code'] = code
     return redirect(
-        client.build_auth_url(config.auth_url, code, config.redirect_uri)
+        client.build_auth_url(config.authorize_url, code, config.redirect_uri)
     )
 
 @app.route("/callback")
 def callback():
-    return 'returned!'
+    code = session['code']
+    access_token = client.get_token(config.oauth_authorize_url, config.consumer_key, code)
+    pocket_count = client.get_count(config.list_url, access_token, config.consumer_key)
+    return 'Your pocket size: ' + str(pocket_count) + ' <a href="/">again</a>'
+    #return 'returned and we have a token: ' + access_token
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4242, debug=False)
+    app.secret_key = config.session_secret
+    app.run(host='0.0.0.0', port=4242, debug=False, threaded=True)
--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import urllib
 import urllib2
 import urlparse
+import json
 
 def get_request_token(code_url, consumer_key, redirect_uri):
 
@@ -22,3 +23,30 @@ def build_auth_url(auth_url, code, redirect_uri):
         'redirect_uri': redirect_uri
     }
     return auth_url + '?' + urllib.urlencode(auth_params)
+
+def get_token(auth_url, consumer_key, code):
+    post_params = {
+        'consumer_key': consumer_key,
+        'code': code
+    }
+
+    request = urllib2.Request(auth_url, urllib.urlencode(post_params))
+    response = urllib2.urlopen(request)
+
+    raw_response = response.read()
+    response_body = dict(urlparse.parse_qsl(raw_response))
+    return response_body.get('access_token')
+
+def get_count(list_url, access_token, consumer_key):
+    list_params = {
+        'access_token': access_token,
+        'consumer_key': consumer_key,
+        'count': 10000
+    }
+    request = urllib2.Request(list_url, urllib.urlencode(list_params))
+    response = urllib2.urlopen(request)
+
+    raw_response = response.read()
+    response_data = json.loads(raw_response)
+
+    return len(response_data.get('list'))
