c0e2516dc3d76dbbc725f90a3ac1376bb5bdbe67
cece <heilig.szabolcs@ustream.tv>
2016-07-17 14:13:39 +0200

    auth step 1

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect
 import sys
-
+import config
+import client
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "It works!"
+	code = client.get_request_token(config.code_url, config.consumer_key, config.redirect_uri)
+	return redirect(
+		client.build_auth_url(config.auth_url, code, config.redirect_uri)
+	)
+
+@app.route("/callback")
+def callback():
+	return 'returned!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4242, debug=False)
---
+++ config.py
@@ -0,0 +1,5 @@
+code_url = 'https://getpocket.com/v3/oauth/request'
+auth_url = 'https://getpocket.com/auth/authorize'
+
+consumer_key = '21077-83a33adfacd5e42b99092773'
+redirect_uri = 'http://127.0.0.1:4242/callback'
